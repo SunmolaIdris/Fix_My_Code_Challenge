f3692e645bddd7c45f3f4f9260e67d2d5a8b64ec
SunmolaIdris <sunmolaidris@gmail.com>
2024-01-23 05:27:22 -0800

    retry task

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -13,7 +13,7 @@ def fizzbuzz(n):
 		- For numbers which are multiples of both three and five print "FizzBuzz".
 		"""
 		if n < 1:
-			return
+		return
 
 		tmp_result = []
 		for i in range(1, n + 1):
@@ -28,12 +28,12 @@ def fizzbuzz(n):
 		print(" ".join(tmp_result))
 
 
-			if __name__ == '__main__':
-				if len(sys.argv) <= 1:
-					print("Missing number")
-					print("Usage: ./0-fizzbuzz.py <number>")
-					print("Example: ./0-fizzbuzz.py 89")
-					sys.exit(1)
+if __name__ == '__main__':
+	if len(sys.argv) <= 1:
+		print("Missing number")
+		print("Usage: ./0-fizzbuzz.py <number>")
+		print("Example: ./0-fizzbuzz.py 89")
+		sys.exit(1)
 
-				number = int(sys.argv[1])
-				fizzbuzz(number)
+	number = int(sys.argv[1])
+	fizzbuzz(number)
